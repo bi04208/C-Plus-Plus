e2e1c878341b566ca7f38d86770e370152f1a048
bi04208 <bi04208@kookmin.ac.kr>
2021-11-12 16:56:51 +0900

    sha256 first commit

---
+++ hashing/sha256.cpp
@@ -0,0 +1,25 @@
+
+
+#include <algorithm>  /// For std::copy
+#include <array>      /// For std::array
+#include <cassert>    /// For assert
+#include <cstring>    /// For std::memcopy
+#include <iostream>   /// For IO operations
+#include <string>     /// For strings
+#include <vector>     /// For std::vector
+
+/**
+ * @namespace hashing
+ * @brief Hashing algorithms
+ */
+namespace hashing {
+
+/**
+ * @namespace SHA-1
+ * @brief Functions for the [SHA-1](https://en.wikipedia.org/wiki/SHA-1)
+ * algorithm implementation
+ */
+
+namespace sha256 {}  // namespace sha256
+
+}  // namespace hashing
